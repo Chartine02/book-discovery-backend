d49c0f516c8ac9e467cc88418f10a2d402ff030e
Chartine02 <noellachartine125@gmail.com>
2026-02-19 15:00:00 +0200

    project setup

---
+++ src/main/java/com/noella/bookdiscovery/BookDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.noella.bookdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noella/bookdiscovery/BookDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noella.bookdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
